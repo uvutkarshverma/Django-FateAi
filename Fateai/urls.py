b40f54271ad6f1879ab19fbb15bf2ae29e47a396
Utkarsh Verma <utkarsh.verma9598@gmail.com>
2023-12-29 06:33:47 +0530

    fateai2

--- Fateai/urls.py
+++ Fateai/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+
 
 urlpatterns = [
     path("", include("webapp.urls")),
